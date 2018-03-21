68cb257a741fe6eda4055a1e8255cbb6e83c6efa
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-21 02:05:42 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,70 +6,6 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do
-<<<<<<< HEAD
-    erb :'/pets/new'
-=======
-        erb :'/pets/new'
->>>>>>> 9a62368d0b4d5c9429fff27ce77f0ac7df726779
-  end
-
-  post '/pets' do
-    @pet = Pet.create(params[:pet])
-<<<<<<< HEAD
-    if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner"]["name"])
-    end
-    @pet.save
-    redirect "pets/#{@pet.id}"
-=======
-      if !params["owner"]["name"].empty?
-        @pet.owner = Owner.create(name: params["owner"]["name"])
-      end
-      @pet.save
-    redirect to "pets/#{@pet.id}"
->>>>>>> 9a62368d0b4d5c9429fff27ce77f0ac7df726779
-  end
-
-  get '/pets/:id' do
-    @pet = Pet.find(params[:id])
-    erb :'/pets/show'
-  end
-  get '/pets/:id/edit' do
-    @pet= Pet.find(params[:id])
-    erb :'/pets/edit'
-  end
-
-  post '/pets/:id' do
-
-    @pet = Pet.find(params[:id])
-    @pet.update(params["pet"])
-    if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner"]["name"])
-    end
-    @pet.save
-    redirect "pets/#{@pet.id}"
-  end
-
-  get '/pets/:id/edit' do
-    @pet = Pet.find(params[:id])
-    erb :'/pets/edit'
-
-    binding.pry
-    @pet = Pet.find(params[:id])
-       @pet.update(params["pet"])
-       if !params["owner"]["name"].empty?
-         @pet.owner = Owner.create(name: params["owner"]["name"])
-       end
-       @pet.save
-    redirect to "pets/#{@pet.id}"
->>>>>>> 9a62368d0b4d5c9429fff27ce77f0ac7df72677class PetsController < ApplicationController
-
-  get '/pets' do
-    @pets = Pet.all
-    erb :'/pets/index'
-  end
-
-  get '/pets/new' do
     erb :'/pets/new'
   end
 
@@ -100,7 +36,5 @@ class PetsController < ApplicationController
   get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
     erb :'/pets/edit'
-  end
-end9
   end
 end
